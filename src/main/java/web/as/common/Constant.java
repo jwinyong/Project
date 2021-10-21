76b2badcbe1b443d1b67c8982510e2d5f6330427
jwinyong <jwinyong@naver.com>
2021-10-21 19:24:13 +0900

    11

--- src/main/java/web/as/common/Constant.java
+++ src/main/java/web/as/common/Constant.java
@@ -8,9 +8,9 @@ public class Constant {
 	public static final String EXCEL_BANKING = "EXCEL_BANKING";	//리포트 뱅킹 엑셀.
 	
 	//개발
-	public static final String MAIL_SENDER = "jwinyong@gmail.com";		//발송자 메일
-	public static final String MAIL_ID     = "jwinyong@gmail.com";		//GMail 계정
-	public static final String MAIL_PWD    = "dustmq0901";				//Gmail 패스워드
+	public static final String MAIL_SENDER = "volice1@gmail.com";		//발송자 메일
+	public static final String MAIL_ID     = "volice1@gmail.com";		//GMail 계정
+	public static final String MAIL_PWD    = "Wlsgudaos12!@";			//Gmail 패스워드
 	
 	public static final String MAIL_SENDER_NM	= "자바강의";			//발신자 명
 	
